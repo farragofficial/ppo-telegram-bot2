4f77c3eab9205d68ceed4cfd89c53ee79ca6c29c
farragofficial <farragofficial@gmail.com>
2025-10-26 00:18:09 +0300

    Add files via upload

---
+++ install_deps.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+apt-get update -y
+apt-get install -y \
+  libnss3 \
+  libnspr4 \
+  libatk1.0-0 \
+  libatk-bridge2.0-0 \
+  libcups2 \
+  libdrm2 \
+  libdbus-1-3 \
+  libxkbcommon0 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  libgbm1 \
+  libxshmfence1 \
+  libasound2 \
+  libpangocairo-1.0-0 \
+  libpango-1.0-0 \
+  libcairo2 \
+  libxext6 \
+  libxfixes3 \
+  libglib2.0-0 \
+  libgobject-2.0-0
